091709b8e406e13e0f4bebb458228e3077399fb9
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-05-24 02:53:16 +0300

    self hosting project

---
+++ Zza.SelfHost/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ServiceModel;
+using Zza.Services;
+
+namespace Zza.SelfHost
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var host = new ServiceHost(typeof(ZzaService));
+            host.Open();
+            Console.WriteLine("Hit any key to exit");
+            Console.ReadKey();
+
+            host.Close();
+        }
+    }
+}
