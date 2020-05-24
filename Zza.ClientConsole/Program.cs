8c2fc3ef2bef4e467581d5bfbf2fe910c5d965c3
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-05-25 02:26:01 +0300

    using custom proxy class

--- Zza.ClientConsole/Program.cs
+++ Zza.ClientConsole/Program.cs
@@ -11,11 +11,18 @@ namespace Zza.ClientConsole
             //var client = new ZzaServiceClient("BasicHttpBinding_IZzaService");
             //var products = client.GetProducts();
 
-            var proxy = new ZzaServiceClient("BasicHttpBinding_IZzaService");
-            var products = await proxy.GetProductsAsync();
-            foreach (var product in products)
+            //var proxy = new ZzaServiceClient("BasicHttpBinding_IZzaService");
+            //var products = await proxy.GetProductsAsync();
+            //foreach (var product in products)
+            //{
+            //    Console.WriteLine($"{product.Id} - {product.Name}");
+            //}
+
+            var p = new ZzaProxy();
+            var _products = await p.GetProductsAsync();
+            foreach (var _p in _products)
             {
-                Console.WriteLine($"{product.Id} - {product.Name}");
+                Console.WriteLine(_p.Name);
             }
 
             Console.ReadLine();
