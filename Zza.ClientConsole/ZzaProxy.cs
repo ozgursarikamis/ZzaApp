d84174364d7f947c991efd220fd4f565cab30bc3
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-05-25 02:21:04 +0300

    Generating custom Proxy class

---
+++ Zza.ClientConsole/ZzaProxy.cs
@@ -0,0 +1,45 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+using Zza.ClientConsole.ZzaServices;
+using Zza.Entities;
+
+namespace Zza.ClientConsole
+{
+    public class ZzaProxy : ClientBase<IZzaService>, IZzaService
+    {
+        public ZzaProxy() { }
+        public ZzaProxy(string endpointName): base(endpointName) { }
+        public ZzaProxy(Binding binding, string address): base(binding, new EndpointAddress(address)) { }
+
+        public Product[] GetProducts()
+        {
+            return Channel.GetProducts();
+        }
+
+        public Task<Product[]> GetProductsAsync()
+        {
+            return Channel.GetProductsAsync();
+        }
+
+        public Customer[] GetCustomers()
+        {
+            return Channel.GetCustomers();
+        }
+
+        public Task<Customer[]> GetCustomersAsync()
+        {
+            return Channel.GetCustomersAsync();
+        }
+
+        public void SubmitOrder(Order order)
+        {
+            Channel.SubmitOrder(order);
+        }
+
+        public Task SubmitOrderAsync(Order order)
+        {
+            return Channel.SubmitOrderAsync(order);
+        }
+    }
+}
